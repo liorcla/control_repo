660fc78a1af35a676e26220c40fdc726be0e0a19
liorcla <39294852+liorcla@users.noreply.github.com>
2018-05-15 13:01:07 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,3 +4,7 @@ node default {
 node  ip-172-31-27-103.eu-central-1.compute.internal {
   include role:master_server
 }
+node  ip-172-31-20-8.eu-central-1.compute.internal {  
+  include role:app_server
+  include role:db_server
+  }
