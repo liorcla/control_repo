2b2bd89aee4548253025bf2b8b2e56232f56176b
liorcla <39294852+liorcla@users.noreply.github.com>
2018-05-15 13:17:54 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,7 +3,7 @@ node default {
  
 node  'ip-172-31-27-103.eu-central-1.compute.internal' {
   include role::master_server
-  file {'root/README':
+  file {'/root/README':
     ensure =>file.
     content=>$fqdn,
     }
